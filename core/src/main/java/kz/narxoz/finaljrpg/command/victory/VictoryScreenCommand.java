7f2e207c3720368d651ac86c08cb71f2baa135f6
Alan <alanshmyrinsource@gmail.com>
2026-05-21 12:22:21 +0500

    feat: add victory command

---
+++ core/src/main/java/kz/narxoz/finaljrpg/command/victory/VictoryScreenCommand.java
@@ -0,0 +1,21 @@
+package kz.narxoz.finaljrpg.command.victory;
+
+import com.badlogic.gdx.Game;
+import kz.narxoz.finaljrpg.battle.event.VictoryEvent;
+import kz.narxoz.finaljrpg.command.screen.ScreenCommand;
+import kz.narxoz.finaljrpg.screen.VictoryScreen;
+
+public class VictoryScreenCommand extends ScreenCommand {
+    private final VictoryEvent victoryEvent;
+
+    public VictoryScreenCommand(Game game, VictoryEvent victoryEvent) {
+        super(game);
+        this.victoryEvent = victoryEvent;
+    }
+
+    @Override
+    public void execute() {
+        game.setScreen(new VictoryScreen(victoryEvent));
+        history.push(this);
+    }
+}
